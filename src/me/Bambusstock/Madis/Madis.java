54369de5752d0020816a494455253b052c48484c
fzuellich <fzuellich@gmail.com>
2012-02-04 00:34:21 +0100

    adjust version in log

--- src/me/Bambusstock/Madis/Madis.java
+++ src/me/Bambusstock/Madis/Madis.java
@@ -1,12 +1,13 @@
 package me.Bambusstock.Madis;
 import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Madis extends JavaPlugin{
 	Logger log = Logger.getLogger("Minecraft");
 
 	public void onEnable() {
-		log.info("Madis 0.1 enabled. It's magic!");
+		log.info("Madis 0.2 enabled. It's magic!");
 		getServer().getPluginManager().registerEvents(new BlockListener(), this);
 	}
 	
