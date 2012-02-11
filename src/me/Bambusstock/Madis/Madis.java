a1facad7b8653107767db0e599929cd26a9464c8
fzuellich <fzuellich@gmail.com>
2012-02-11 12:23:41 +0100

    implemented configuration

--- src/me/Bambusstock/Madis/Madis.java
+++ src/me/Bambusstock/Madis/Madis.java
@@ -4,15 +4,15 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Madis extends JavaPlugin{
-	Logger log = Logger.getLogger("Minecraft");
+    Logger log = Logger.getLogger("Minecraft");
+    
+    public void onEnable() {
+	log.info("Madis 0.3 enabled. It's magic!");
+	getServer().getPluginManager().registerEvents(new BlockListener(this), this);
+    }
 
-	public void onEnable() {
-		log.info("Madis 0.2 enabled. It's magic!");
-		getServer().getPluginManager().registerEvents(new BlockListener(), this);
-	}
-	
-	public void onDisable() {
-		log.info("Madis disabled. Magic is gone!");
-	}
-	
+    public void onDisable() {
+	log.info("Madis disabled. Magic is gone!");
+    }
+
 }
